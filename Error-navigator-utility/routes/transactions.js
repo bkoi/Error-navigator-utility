6824af3381bdf79038120da2fd45033148234bbb
bkoi <bernadettkoi@gmail.com>
2024-10-20 18:22:36 +0100

    Added try-catch to logic in transactions.js

--- Error-navigator-utility/routes/transactions.js
+++ Error-navigator-utility/routes/transactions.js
@@ -1,41 +1,42 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const connectDB = require('../utils/database');
+
 const router = express.Router();
 
 //View transactions
-/*router.get('/transactions', async (req, res) => {
+router.get('/transactions', async (req, res) => {
     const sql = 'SELECT * FROM transactions';
-    const pool = await connectDB();
 
-    pool.query(sql, (err, results, fields) => {
-        if (!err) {
-            res.send(results);
-        } else {
-            console.log(err);
-        }
-    });
+    try {
+        const pool = await connectDB();
+        const results = await pool.query(sql);
+        return res.status(200).json(results);
+    } catch (error) {
+        console.error('Error fetching transactions:', error);
+        res.status(500).json({ error: 'FInternal Server Error' });
+    }
 });
-*/
 
 //View single transaction
 router.get('/transactions/:messageid', async (req, res) => {
     const { messageid } = req.params;
     const sql = 'SELECT * FROM transactions WHERE messageid = ?';
-    const pool = await connectDB();
+    
+    try {
+        const pool = await connectDB();
+        const results = await pool.query(sql, messageid);
 
-    pool.query(sql, messageid, (err, results, fields) => {
-        if (!err) {
-            if (results.length > 0) {
-                res.send(results);
-            } else {
-                res.status(404).send('Transaction not found');
-            }
+        if (results.length > 0) {
+            return res.status(200).json(results);
         } else {
-            console.log(err);
-            res.status(500).send('Error retrieving transaction');
+            return res.status(404).send('Transaction not found');
         }
-    });
+        
+    } catch (error) {
+        console.error('Error retrieving transaction:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
  });
 
 //Update transaction
@@ -43,35 +44,41 @@ router.put('/transactions/:messageid', async(req, res) => {
     const { messageid } = req.params;
     const { UETR, F20, createddate, event_desc, root_cause, staffid } = req.body;
     const sql = 'UPDATE transactions SET UETR = ?, F20 = ?, createddate = ?, event_desc = ?, root_cause = ?, staffid = ? WHERE messageid = ?';
-    const pool = await connectDB();
+    
+    
+    try {
+        const pool = await connectDB();
+        const results = await pool.query(sql, [UETR, F20, createddate, event_desc, root_cause, staffid, messageid]);
 
-    pool.query(sql, [UETR, F20, createddate, event_desc, root_cause, staffid, messageid], (err, results, fields) => {
-        if (!err) {
-            if (results.affectedRows > 0) {
-                res.send("The data for the selected transaction has been successfully updated");
-            } else {
-                res.status(404).send('Messageid not found');
-            }
+        if (results.affectedRows > 0) {
+            res.status(200).send("The data for the selected transaction has been successfully updated");
         } else {
-            console.log(err);
-            res.status(500).send('Failed to update the transaction');
+            res.status(404).send('Messageid not found');
         }
-    });
+    } catch (error) {
+        console.error('Error updating transaction:', error);
+        res.status(500).json({ error: 'Failed to update the transaction' });
+    }
 });
 
 //Delete transaction
 router.delete('/transactions/:messageid', async(req, res) => {
     const { messageid } = req.params;
     const sql = 'DELETE FROM transactions WHERE messageid = ?';
-    const pool = await connectDB();
 
-    pool.query(sql, messageid, (err, results, fields) => {
-        if (!err) {
-            res.send("The selected transaction has been deleted");
+    try {
+        const pool = await connectDB();
+        const results = await pool.query(sql, [messageid]);
+
+        if (results.affectedRows > 0) {
+            res.status(200).send("The selected transaction has been deleted");
         } else {
-            console.log(err);
+            res.status(404).send('Messageid not found');
         }
-    });
+    } catch (error) {
+        console.error('Error deleting transaction:', error);
+        res.status(500).json({ error: 'Failed to delete the transaction' });
+    }
 });
 
 module.exports = router;
